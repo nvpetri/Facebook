d106234d3a8b6df117dd814774110583b7507684
NPS <nicolaspetri18@gmail.com>
2023-10-04 15:48:31 -0300

    up

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const button = document.getElementById('login')
+
+function teste() {
+    let email = document.getElementById('email').value
+    let senha = document.getElementById('senha').value
+
+    let validaEmail = "nicolaspetri18@gmail.com"
+    let validaSenha = "Nicolas1234"
+
+    if (email && senha) {
+        if (email == validaEmail && senha == validaSenha) {
+            window.location.href = './pg2/index.html'
+        } else {
+            alert('Email e/ou senha incorreto (s)!')
+        }
+    }
+}
+button.addEventListener('click', teste)
