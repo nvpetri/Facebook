0ca175abd4107ead1c39ec63a543c60031b3a41e
nvpetri <nicolaspetri18@gmail.com>
2023-10-05 20:29:59 -0300

    andando

--- pg1_pt/pg2/app.js
+++ pg1_pt/pg2/app.js
@@ -1,29 +1,38 @@
 const apiKey = '41Ap8BBPuKfQeclpjj3Vv3DpEX0JAFFdG4h92EcxL7RIczK0wuoQ6b5v'
 const numberOfImages = 40
-const theme = "bacon"
-const apiUrl = `https://api.pexels.com/v1/search?query=${theme}&per_page=${numberOfImages}`
+const themeEnt = document.getElementById('busca')
+const container = document.getElementById('imagem-container')
 
-fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-            'Authorization': apiKey,
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const photos = data.photos;
-        const container = document.getElementById('imagem-container')
+themeEnt.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        const theme = themeEnt.value
 
-        photos.forEach(photo => {
-            const imgcontainer = document.createElement('div')
-            const imgElement = document.createElement('img')
-            imgcontainer.classList.add('imgContainer')
-            imgElement.src = photo.src.medium
-            imgElement.alt = photo.photographer
-            imgcontainer.appendChild(imgElement)
-            container.appendChild(imgcontainer)
-        });
-    })
-    .catch(error => {
-        console.error('Ocorreu um erro:', error)
-    })
+        const apiUrl = `https://api.pexels.com/v1/search?query=${theme}&per_page=${numberOfImages}`
+
+        fetch(apiUrl, {
+                method: 'GET',
+                headers: {
+                    'Authorization': apiKey,
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                container.innerHTML = ''
+
+                const photos = data.photos
+
+                photos.forEach(photo => {
+                    const imgcontainer = document.createElement('div')
+                    const imgElement = document.createElement('img')
+                    imgcontainer.classList.add('imgContainer')
+                    imgElement.src = photo.src.medium
+                    imgElement.alt = photo.photographer
+                    imgcontainer.appendChild(imgElement)
+                    container.appendChild(imgcontainer)
+                })
+            })
+            .catch(error => {
+                console.error('Ocorreu um erro:', error)
+            })
+    }
+})
